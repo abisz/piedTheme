d6305ce0e2c59924c0e7e164b2d00e8c10eb9271
abisz <reinsperger.smn@gmail.com>
2015-06-29 12:47:13 +0200

    gulp file adapted to the needs of this project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,71 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-    // place code for your default task here
+var env,
+    coffeeSources,
+    jsSources,
+    sassSources,
+    outputDir,
+    sassStyle,
+    cssSources;
+
+env = process.env.NODE_ENV || 'development';
+
+if(env === 'development'){
+    outputDir ='builds/development/';
+    sassStyle = 'expanded';
+}else{
+    outputDir = 'builds/production/';
+    sassStyle = 'compressed';
+}
+
+coffeeSources = ['js/coffee/*.coffee'];
+jsSources = ['js/*.js'];
+sassSources =['css/sass/*.scss'];
+cssSources = ['css/*.css'];
+
+gulp.task('coffee', function(){
+    gulp.src(coffeeSources)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('js/'))
 });
+
+gulp.task('js', function(){
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(browserify())
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulp.dest('js/'))
+})
+
+gulp.task('sass', function(){
+    gulp.src(sassSources)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('sass.css'))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('css', function(){
+    gulp.src(cssSources)
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest(''));
+
+})
+
+gulp.task('watch', function(){
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch(sassSources, ['sass']);
+    gulp.watch(cssSources, ['css']);
+
+
+});
+
+gulp.task('default', ['coffee', 'js', 'sass', 'css', 'watch']);
